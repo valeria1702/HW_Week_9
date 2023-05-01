b4a1ddc0ad0e68f8869bcaea035967f3e84c3cec
valeria1702 <sharova17_00@mail.ru>
2023-05-01 23:56:46 +0300

    add HW week9

---
+++ script.js
@@ -0,0 +1,49 @@
+//Задание №1 
+/*Вариант_1*/
+let cleaner = 40000;
+teacher = 22000;
+console.log(teacher + cleaner)
+/*Вариант_2*/
+console.log(40000 + 22000)
+//Задание №2
+/*Вариант_1*/
+let word = 'Владивосток';
+n = 2000;
+console.log(word + " " + n)
+/*Вариант_2*/
+console.log('Владивосток' + " " + n)
+//Задание №3
+/*Вариант_1*/
+let messenger = 128;
+photo = 254;
+vk = 137;
+youtube = 201;
+console.log(messenger + photo + vk + youtube)
+/*Вариант_2*/
+console.log(128 + 254 + 137 + 201)
+//Задание №4
+console.log(2 ** 5)
+//Задание №5
+console.log("Нужно бежать со всех ног, чтобы только оставаться на месте, а чтобы куда-то попасть, надо бежать как минимум вдвое быстрее!")
+//Задание №6
+const greetings = "Привет";
+const name = "Кот";
+console.log(greetings + ", " + name)
+//Задание №7
+/*1*/
+let time;
+time = "34";
+console.log('Старт поездки. Осталось минут: ' + time)
+/*2*/
+time -= 15;
+console.log('Немного сториз в соцсетях. Осталось минут: ' + time)
+/*3*/
+time -= 10;
+console.log('Немного не новостей, но событий. Осталось минут: ' + time)
+/*4*/
+time = 0;
+console.log('Вы приехали. Добро пожаловать в Москву')
+//Задание №8
+let fahrenheit = 451;
+celsius = (fahrenheit - 32) / 1.8;
+console.log(`${fahrenheit} градуса по Фаренгейту - это ${celsius} градуса по Цельсию.`)
